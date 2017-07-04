b7c0196ae9c29b2f579776e66af6328be6a64c58
Yhovanny Quintero <yqtronto@hotmail.com>
2017-07-04 01:03:58 -0400

    Creado fv_mod_animals
    Se ha creado el mod fv_mod_animals que permite tener mejor producción en los animales.

---
+++ fv_mod_animals/script.lua
@@ -0,0 +1,30 @@
+
+-- >>> Animales Domesticos<<<
+-- fv.core.animalsConfig.cow.resourceOut = {meat = 50,skin = 5}
+-- fv.core.animalsConfig.cow.production = {milk = 2}
+fv.core.animalsConfig.cow.productionTimer = 210000
+fv.core.animalsConfig.cow.EatPerYear = 26.65
+
+fv.core.animalsConfig.pig.EatPerYear = 30.0
+
+-- fv.core.animalsConfig.sheep.resourceOut = {meat = 8,wool = 20,skin = 3}
+-- fv.core.animalsConfig.sheep.production = {wool = 2,milk = 1}
+fv.core.animalsConfig.sheep.productionTimer = 285000
+fv.core.animalsConfig.sheep.EatPerYear = 13.35
+
+-- fv.core.animalsConfig.chicken.resourceOut = {meat = 2}
+-- fv.core.animalsConfig.chicken.production = {eggs = 2}
+fv.core.animalsConfig.chicken.productionTimer = 600000
+fv.core.animalsConfig.chicken.EatPerYear = 8.0
+
+-- fv.core.animalsConfig.donkey.resourceOut = {meat = 20}
+fv.core.animalsConfig.donkey.EatPerYear = 6.15
+-- fv.core.animalsConfig.donkey.speed = 1.2
+
+-- >>> Animales Salvajes <<<
+-- fv.core.animalsConfig.deer.resourceOut = {meat = 20,skin = 5}
+-- fv.core.animalsConfig.fox.resourceOut = {meat = 2,skin = 2}
+-- fv.core.animalsConfig.wolf.resourceOut = {meat = 5,skin = 3}
+-- fv.core.animalsConfig.moose.resourceOut = {meat = 25,skin = 5}
+-- fv.core.animalsConfig.bear.resourceOut = {meat = 30,skin = 5}
+-- fv.core.animalsConfig.boar.resourceOut = {meat = 15,skin = 3}
